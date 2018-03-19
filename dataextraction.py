9147dd01a70ea549abea97bdeaec5dacc366ec21
Birgit Sutzl <bss116@ic.ac.uk>
2018-03-19 15:20:31 +0000

    getting all loss data in one dic

--- dataextraction.py
+++ dataextraction.py
@@ -2,46 +2,60 @@ import csv
 import numpy as np
 
 
-def extract_data(filename):
+def extract_data(files):
     # First row of file is used to create the keywords for the subsequent rows that
     # are read in as a dictionary
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        data = {f: [] for f in reader.fieldnames if f}
-        for row in reader:
-            for field, value in row.items():
-                if field:
-                    data[field].append(value)
+    data = []
+    initialise = True
+    for filename in files:
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            if initialise is True:
+                data = {f: [] for f in reader.fieldnames if f}
+                initialise = False
+            for row in reader:
+                for field, value in row.items():
+                    if field:
+                        data[field].append(value)
+
     for fieldname in data:
         data[fieldname] = np.array(data[fieldname])
     return data
 
 
 def get_test_data(data):
-    np.random.seed(42)
-    length = 0
-    for fieldname in data:
-        length = len(data[fieldname])
+    # get length of fist column
+    n = 0
+    for firstcolumn in data:
+        n = len(data[firstcolumn])
         break
-    indices = np.random.choice(length - 1, int(0.2 * (length - 1)), replace=False)
+
+    # get 20 percent of indices
+    indices = get_datasplit_indices(n)
+
+    # remove data of indices
     for fieldname in data:
         data[fieldname] = np.delete(data[fieldname], indices)
     return data
 
 
 def get_validation_data(data):
-    np.random.seed(42)
-    length = 0
-    for fieldname in data:
-        length = len(data[fieldname])
+    # get length of fist column
+    n = 0
+    for firstcolumn in data:
+        n = len(data[firstcolumn])
         break
-    indices = np.random.choice(length - 1, int(0.2 * (length - 1)), replace=False)
+
+    # get 20 percent of indices
+    indices = get_datasplit_indices(n)
+
+    # get data of indices
     for fieldname in data:
         data[fieldname] = data[fieldname][indices]
     return data
 
-# filename = "AIR_data/Loss_data/Complete/Region_1_DR.csv"
-# data = extract_data(filename)
-# print(len(data['AIRSID']))
-# data_test = get_test_data(data)
-# print(len(data['AIRSID']))
+
+def get_datasplit_indices(n):
+    np.random.seed(42)
+    indices = np.random.choice(n - 1, int(0.2 * (n - 1)), replace=False)
+    return indices
--- test_data_input.py
+++ test_data_input.py
@@ -1,11 +1,24 @@
+import glob
 from dataextraction import *
-from jamboree import *
+# from jamboree import *
 
 if __name__ == '__main__':
+    # # get all files
+    # lossfiles = glob.glob("AIR_data/Loss_Data/Complete/Region_*_DR.csv")
+    # lossfiles = sorted(lossfiles)
+    # print(lossfiles)
+    # data = extract_data(lossfiles)
+    # print(data.keys())
 
-    filename = "AIR_data/Loss_data/Complete/Region_1_DR.csv"
+    filename = "AIR_data/Loss_Data/Complete/Region_1_DR.csv"
     data = extract_data(filename)
-    print(len(data['AIRSID']))
     print(data.keys())
-    data_test = get_test_data(data)
-    print(len(data['AIRSID']))
+
+    # filename = "AIR_data/Predictor_Data/GEM_HistoricalFreq.csv"
+    # data = extract_data(filename)
+    # print(data.keys())
+
+    # print(len(data['AIRSID']))
+    # print(data.keys())
+    # data_test = get_test_data(data)
+    # print(len(data['AIRSID']))
