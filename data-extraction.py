577500c6307a0c61cccb1946eb817031fded7b6c
Ben Snowball <bss16@ic.ac.uk>
2018-03-19 13:57:17 +0000

    File now successfully reads in correct data

--- data-extraction.py
+++ data-extraction.py
@@ -1,22 +1,46 @@
 import csv, random
+import numpy as np
 
 
 def extract_data(filename):
-     length = 100000
-     airsid = zeros(length)
-     lob1 = zeros(length)
      # First row of file is used to create the keywords for the subsequent rows that
      # are read in as a dictionary
-     i = 0
-     with open('example.csv') as csvfile:
+     with open(filename) as csvfile:
           reader = csv.DictReader(csvfile)
+          data = {f: [] for f in reader.fieldnames if f}
           for row in reader:
-              airsid[i] = row['AIRSID']
-              lob1[i] = row['LOB1']
-              i++
-     # We only want 20% of the data
-     indices = random.sample(range(1, length), int(round(0.2*length)))
-     return airsid[indices], lob1[indices]
+               for field, value in row.items():
+                    if field:
+                         data[field].append(value)
+     for fieldname in data:
+          data[fieldname] = np.array(data[fieldname])
+     return data
+
+def get_test_data(data):
+     np.random.seed(42)
+     length = 0
+     for fieldname in data:
+          length = len(data[fieldname])
+          break
+     indices = np.random.choice(length-1, int(0.2*(length-1)), replace=False)
+     for fieldname in data:
+          data[fieldname] = np.delete(data[fieldname], indices)
+     return data
+
+def get_validation_data(data):
+     np.random.seed(42)
+     length = 0
+     for fieldname in data:
+          length = len(data[fieldname])
+          break
+     indices = np.random.choice(length-1, int(0.2*(length-1)), replace=False)
+     for fieldname in data:
+          data[fieldname] = data[fieldname][indices]
+     return data
+
 
 filename = "AIR_data/Loss_data/Complete/Region_1_DR.csv"
-airsid, lobs1 = extract_data(filename)
+data = extract_data(filename)
+print len(data['AIRSID'])
+data_test = get_test_data(data)
+print len(data['AIRSID'])
