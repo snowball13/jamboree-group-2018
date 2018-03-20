61ab4ccb97b4b7a2950daea8eca8e82db8ddcdc4
Birgit Sutzl <bss116@ic.ac.uk>
2018-03-20 10:46:19 +0000

    working on all predictor data

--- test_data_input.py
+++ test_data_input.py
@@ -7,18 +7,33 @@ if __name__ == '__main__':
     lossfiles = glob.glob("AIR_data/Loss_Data/Complete/Region_*_DR.csv")
     lossfiles = sorted(lossfiles)
     print(lossfiles)
-    data_lob1 = extract_data(lossfiles)
-    print(data_lob1.keys())
+
+    data_loss = extract_data(lossfiles)
+    print(data_loss.keys())
+
+    region_ids = get_ids_list(data_loss)
+    print(len(region_ids))
 
     # filename = "AIR_data/Loss_Data/Complete/Region_1_DR.csv"
     # data_lob1 = extract_data([filename])
     # print(data_lob1.keys())
+    #
+    # filename = "AIR_data/Predictor_Data/GEM_HistoricalFreq.csv"
+    # data_pred = extract_data([filename])
+    # print(data_pred.keys())
 
-    filename = "AIR_data/Predictor_Data/GEM_HistoricalFreq.csv"
-    data_pred = extract_data([filename])
+    # ids, avg = get_average_lob1(data_loss)
+
+    predfiles = glob.glob("AIR_data/Predictor_Data/*.csv")
+    predfiles = sorted(predfiles)
+    print(predfiles)
+
+    data_pred = extract_data(predfiles)
     print(data_pred.keys())
 
-    ids, avg = get_average_lob1(data_lob1)
+    pred_ids = get_ids_list(data_loss)
+    print(len(pred_ids))
+
     # ids, avg, pred = get_correct_data_arrays(ids, avg, data_pred)
     # print len(ids), len(avg), len(pred)
 
